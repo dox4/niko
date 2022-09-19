36ce5f5e8e7113d1ec49da593109b43d210d9fcf
dox4 <dox4@foxmail.com>
2022-09-20 01:59:34 +0800

    feat/fs: some db operations;

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,9 +1,11 @@
-use tracing::debug;
+use notify::event::{CreateKind, ModifyKind, RemoveKind};
+use notify::Watcher;
+use tracing::*;
 use walkdir::WalkDir;
 
 use crate::config::NiKoDirConfig;
 use crate::model::file;
-use std::os::unix::fs::PermissionsExt;
+use std::sync::mpsc;
 
 pub async fn may_need_scanning(conf: &NiKoDirConfig<'_>) -> Result<(), sqlx::Error> {
     for entry in WalkDir::new(conf.full_path())
@@ -13,47 +15,20 @@ pub async fn may_need_scanning(conf: &NiKoDirConfig<'_>) -> Result<(), sqlx::Err
     {
         let parent = entry.path().parent().unwrap().display().to_string();
         let name: String = entry.file_name().to_string_lossy().into();
+        let md = entry.metadata().unwrap();
         match file::fetch_one_by_path(parent.clone(), name.clone()).await {
             Ok(fetched) => {
-                let f = file::File {
-                    id: fetched.id,
-                    parent: parent,
-                    name: name,
-                    is_dir: entry.file_type().is_dir(),
-                    size: if entry.file_type().is_dir() {
-                        0
-                    } else {
-                        entry.metadata().unwrap().len() as i32
-                    },
-                    permission: entry.metadata().unwrap().permissions().mode() as i32,
-                    created_at: entry.metadata().unwrap().created().unwrap().into(),
-                    updated_at: entry.metadata().unwrap().modified().unwrap().into(),
-                    deleted_at: None,
-                };
-                let effected = file::update_by_id(f).await?;
-                if effected != 1 {
-                    panic!("update by id effected rows...")
-                }
+                let mut f = file::Entry::from_metadata(md, parent, name);
+                f.id = fetched.id;
+                file::update_by_id(f)
+                    .await
+                    .and_then(|x| Ok(assert_eq!(x, 1, "rows affected more than 1: {:?}", x)))
+                    .unwrap()
             }
             Err(sqlx::Error::RowNotFound) => {
-                let f = file::File {
-                    id: 0,
-                    parent: parent,
-                    name: name,
-                    is_dir: entry.file_type().is_dir(),
-                    size: if entry.file_type().is_dir() {
-                        0
-                    } else {
-                        entry.metadata().unwrap().len() as i32
-                    },
-                    permission: entry.metadata().unwrap().permissions().mode() as i32,
-                    created_at: entry.metadata().unwrap().created().unwrap().into(),
-                    updated_at: entry.metadata().unwrap().modified().unwrap().into(),
-                    deleted_at: None,
-                };
+                let f = file::Entry::from_metadata(md, parent, name);
                 debug!("create file {:?}", f);
                 let id = file::create(f).await?;
-
                 debug!("file record created.... {:?}", file::fetch_by_id(id).await?);
             }
             Err(err) => panic!("fetch record at error {:?}", err),
@@ -62,4 +37,51 @@ pub async fn may_need_scanning(conf: &NiKoDirConfig<'_>) -> Result<(), sqlx::Err
     Ok(())
 }
 
-pub fn start_notify(conf: &NiKoDirConfig) {}
+async fn process_remove_event(
+    rk: RemoveKind,
+    event: notify::event::Event,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match rk {
+        notify::event::RemoveKind::File => todo!(),
+        notify::event::RemoveKind::Folder => todo!(),
+        _ => warn!("unhandled remove event occurred: {:?}", event),
+        // notify::event::RemoveKind::Other => todo!(),
+        // notify::event::RemoveKind::Any => todo!(),
+    };
+    Ok(())
+}
+
+async fn process_event(event: notify::event::Event) -> Result<(), Box<dyn std::error::Error>> {
+    match event.kind.clone() {
+        notify::EventKind::Create(_) | notify::EventKind::Modify(_) => {
+            for file_path in event.paths {
+                let parent = file_path.parent().unwrap().to_string_lossy().into();
+                let name = file_path.file_name().unwrap().to_string_lossy().into();
+                let md = std::fs::metadata(file_path).unwrap();
+                let f = file::Entry::from_metadata(md, parent, name);
+                file::create_or_update(f).await?;
+            }
+        }
+        notify::EventKind::Remove(rk) => process_remove_event(rk, event).await?,
+        _ => warn!("unhandled event kind received: {:?}", event),
+    };
+    Ok(())
+}
+
+pub async fn start_notify(conf: &NiKoDirConfig<'_>) -> Result<(), Box<dyn std::error::Error>> {
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = notify::recommended_watcher(tx)?;
+    watcher.watch(
+        std::path::Path::new(conf.full_path()),
+        notify::RecursiveMode::Recursive,
+    )?;
+    loop {
+        match rx.recv() {
+            Ok(result) => match result {
+                Ok(event) => process_event(event).await?,
+                Err(err) => error!("received an error as an event: {:?}", err),
+            },
+            Err(err) => error!("received an error while watching the path: {:?}", err),
+        }
+    }
+}
--- src/model/file.rs
+++ src/model/file.rs
@@ -1,11 +1,11 @@
 use serde_derive::Serialize;
 use sqlx::types::chrono::{DateTime, Local};
+use std::{fs::Metadata, os::unix::fs::PermissionsExt, path::Path};
 
 use crate::db;
 
 #[derive(Debug, Serialize, Default, Clone, sqlx::FromRow)]
-pub struct File {
-    #[sqlx()]
+pub struct Entry {
     pub id: i32,
     pub parent: String,
     pub name: String,
@@ -17,10 +17,33 @@ pub struct File {
     pub deleted_at: Option<DateTime<Local>>,
 }
 
-pub async fn paged_fetch(page_num: u64, page_size: u64) -> Result<Vec<File>, sqlx::Error> {
+impl Entry {
+    pub fn from_metadata(md: Metadata, parent: String, name: String) -> Self {
+        Self {
+            id: 0,
+            parent,
+            name,
+            is_dir: md.is_dir(),
+            size: md.len() as i32,
+            permission: md.permissions().mode() as i32,
+            created_at: md.created().unwrap().into(),
+            updated_at: md.modified().unwrap().into(),
+            deleted_at: None,
+        }
+    }
+
+    pub fn full_path(&self) -> String {
+        Path::new(self.parent.as_str())
+            .join(self.name.clone())
+            .to_string_lossy()
+            .into()
+    }
+}
+
+pub async fn paged_fetch(page_num: u64, page_size: u64) -> Result<Vec<Entry>, sqlx::Error> {
     let offset = (page_num - 1) * page_size;
     let limit = offset + page_size;
-    let rows = sqlx::query_as::<_, File>("SELECT * FROM files where deleted_at IS NULL LIMIT ?, ?")
+    let rows = sqlx::query_as::<_, Entry>("SELECT * FROM files where deleted_at IS NULL LIMIT ?, ?")
         .bind(offset)
         .bind(limit)
         .fetch_all(db::global_pool())
@@ -28,7 +51,27 @@ pub async fn paged_fetch(page_num: u64, page_size: u64) -> Result<Vec<File>, sql
     Ok(rows)
 }
 
-pub async fn create(file: File) -> Result<u64, sqlx::Error> {
+pub enum CreateOrUpdate {
+    Create(u64),
+    Update(u64),
+}
+
+pub async fn create_or_update(mut file: Entry) -> Result<CreateOrUpdate, sqlx::Error> {
+    let row = fetch_one_by_path(file.parent.clone(), file.name.clone()).await;
+    match row {
+        Ok(file_in_db) => {
+            file.id = file_in_db.id;
+            Ok(CreateOrUpdate::Update(update_by_id(file).await?))
+        }
+        Err(sqlx::Error::RowNotFound) => Ok(CreateOrUpdate::Create(create(file).await?)),
+        Err(err) => {
+            tracing::error!("error occurred while quering by path: {:?}", err);
+            Err(err)
+        }
+    }
+}
+
+pub async fn create(file: Entry) -> Result<u64, sqlx::Error> {
     Ok(sqlx::query(
         r#"INSERT INTO `files`
         (`parent`, `name`, `is_dir`, `size`, `permission`, `created_at`, `updated_at`)
@@ -46,17 +89,17 @@ pub async fn create(file: File) -> Result<u64, sqlx::Error> {
     .last_insert_id())
 }
 
-pub async fn fetch_by_id(id: u64) -> Result<File, sqlx::Error> {
+pub async fn fetch_by_id(id: u64) -> Result<Entry, sqlx::Error> {
     Ok(
-        sqlx::query_as::<_, File>("SELECT * FROM `files` WHERE `deleted_at` IS NULL AND `id` = ?")
+        sqlx::query_as::<_, Entry>("SELECT * FROM `files` WHERE `deleted_at` IS NULL AND `id` = ?")
             .bind(id)
             .fetch_one(db::global_pool())
             .await?,
     )
 }
 
-pub async fn fetch_one_by_path(parent: String, name: String) -> Result<File, sqlx::Error> {
-    Ok(sqlx::query_as::<_, File>(
+pub async fn fetch_one_by_path(parent: String, name: String) -> Result<Entry, sqlx::Error> {
+    Ok(sqlx::query_as::<_, Entry>(
         "SELECT * FROM `file` WHERE `deleted_at` IS NULL AND `parent` = ? AND `name` = ?",
     )
     .bind(parent)
@@ -65,7 +108,7 @@ pub async fn fetch_one_by_path(parent: String, name: String) -> Result<File, sql
     .await?)
 }
 
-pub async fn update_by_id(f: File) -> Result<u64, sqlx::Error> {
+pub async fn update_by_id(f: Entry) -> Result<u64, sqlx::Error> {
     let upd_sql = r#"UPDATE `files` SET
         `parent` = ?,
         `name` = ?,
@@ -84,6 +127,33 @@ pub async fn update_by_id(f: File) -> Result<u64, sqlx::Error> {
         .bind(f.created_at)
         .bind(f.updated_at)
         .bind(f.id)
+        .execute(db::global_pool())
+        .await?
+        .rows_affected())
+}
+
+pub async fn delete_by_path(file: Entry) -> Result<u64, sqlx::Error> {
+    let del_sql = r#"UPDATE `files` SET
+    deleted_at = ?
+    WHERE `parent` = ? AND `name` = ? AND deleted_at IS NULL"#;
+    let now = Local::now();
+    Ok(sqlx::query(del_sql)
+        .bind(Some(now.clone()))
+        .bind(file.parent)
+        .bind(file.name)
+        .execute(db::global_pool())
+        .await?
+        .rows_affected())
+}
+
+pub async fn delete_by_parent(entry: Entry) -> Result<u64, sqlx::Error> {
+    let del_sql = r#"UPDATE `files` SET
+    deleted_at = ?
+    WHERE `parent` = ? AND deleted_at IS NULL"#;
+    let now = Local::now();
+    Ok(sqlx::query(del_sql)
+        .bind(Some(now.clone()))
+        .bind(entry.full_path())
         .execute(db::global_pool())
         .await?
         .rows_affected())
